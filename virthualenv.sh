76eeaa18e1acba12baa570d0685831caae44dfd3
paczesiowa <paczesiowa@gmail.com>
2011-07-12 13:00:36 +0000

    rollback 'add haskell98 to BOOT_PACKAGES and register packages with --force'

--- virthualenv.sh
+++ virthualenv.sh
@@ -32,7 +32,7 @@ CABAL_CONFIG="${CABAL_DIR}/config"
 VIRTHUALENV_BIN_DIR="${VIRTHUALENV_DIR}/bin"
 ACTIVATE_SCRIPT="${VIRTHUALENV_BIN_DIR}/activate"
 CABAL_WRAPPER="${VIRTHUALENV_BIN_DIR}/cabal"
-BOOT_PACKAGES="ffi rts ghc-prim integer-gmp base array containers filepath old-locale old-time unix directory pretty process Cabal bytestring ghc-binary bin-package-db hpc template-haskell ghc time random haskell98"
+BOOT_PACKAGES="ffi rts ghc-prim integer-gmp base array containers filepath old-locale old-time unix directory pretty process Cabal bytestring ghc-binary bin-package-db hpc template-haskell ghc"
 
 mkdir "${VIRTHUALENV}"
 mkdir "${VIRTHUALENV_DIR}"
@@ -42,7 +42,7 @@ mkdir "${VIRTHUALENV_BIN_DIR}"
 ghc-pkg init "${GHC_PACKAGE_PATH}"
 
 for package in ${BOOT_PACKAGES}; do
-    ghc-pkg describe "${package}" | (GHC_PACKAGE_PATH="${GHC_PACKAGE_PATH}" ghc-pkg register --force -)
+    ghc-pkg describe "${package}" | (GHC_PACKAGE_PATH="${GHC_PACKAGE_PATH}" ghc-pkg register -)
 done
 
 cat "${CABAL_CONFIG_SKEL}" | sed "s:<CABAL_DIR>:${CABAL_DIR}:g" | sed "s:<GHC_PACKAGE_PATH>:${GHC_PACKAGE_PATH}:g" > "${CABAL_CONFIG}"
