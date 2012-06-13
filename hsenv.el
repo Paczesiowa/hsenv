ce6fce45abeb23449d78879f2379b744986aa0c9
Nicolas Dudebout <nicolas.dudebout@gatech.edu>
2012-06-13 15:38:21 -0400

    Completely reorganized hsenv.el to offer easier selection of environments.

--- hsenv.el
+++ hsenv.el
@@ -1,69 +1,79 @@
-(setq hsenv-activated nil)
-(setq hsenv nil)
-(setq hsenv-path-backup nil)
-(setq hsenv-exec-path-backup nil)
+(defvar hsenv-active-environment nil)
+(defconst hsenv-path-prepend-file "path_var_prependix")
+(defconst hsenv-ghc-package-path-file "ghc_package_path_var")
 
-(defun hsenv-read-file (fpath)
+(defun hsenv-valid-dirp (hsenv-dir)
+  (let ((valid (and (file-accessible-directory-p hsenv-dir)
+                    (file-readable-p (concat hsenv-dir hsenv-path-prepend-file))
+                    (file-readable-p (concat hsenv-dir hsenv-ghc-package-path-file)))))
+    (when (not valid)
+      (message "The environment you provided is not a valid hsenv directory (%s)." hsenv-dir))
+    valid))
+
+(defun hsenv-is-not-active ()
+  (let ((is-not-active (not hsenv-active-environment)))
+    (when (not is-not-active)
+      (message "An hsenv is already activated (%s)." hsenv))
+    is-not-active))
+
+(defun hsenv-is-active ()
+  (let ((is-active hsenv-active-environment))
+    (when (not is-active)
+      (message "No hsenv currently activated."))
+    is-active))
+
+(defun hsenv-read-file-content (hsenv-dir file)
   (with-temp-buffer
-    (insert-file-contents fpath)
+    (insert-file-contents (concat hsenv-dir path-prepend-file))
     (buffer-string)))
 
 (defun hsenv-activate-environment (hsenv-dir)
   "Activate the Virtual Haskell Environment in directory HSENV-DIR"
-  (let* ((path-var-prependix-file (concat hsenv-dir "path_var_prependix"))
-         (ghc-package-path-var-file (concat hsenv-dir "ghc_package_path_var")))
-    (if (and (file-accessible-directory-p hsenv-dir)
-             (file-readable-p path-var-prependix-file)
-             (file-readable-p ghc-package-path-var-file))
-        (let* ((path-var-prependix (hsenv-read-file path-var-prependix-file))
-               (ghc-package-path-var (hsenv-read-file ghc-package-path-var-file))
-               (new-path-var (concat path-var-prependix ":" (getenv "PATH")))
-               (exec-path-prependix (split-string path-var-prependix ":")))
-          (if (not hsenv-activated)
-              (progn
-                (setq hsenv-path-backup (getenv "PATH"))
-                (setenv "PATH" new-path-var)
-                (setq hsenv-exec-path-backup exec-path)
-                (setq exec-path (append exec-path-prependix exec-path))
-                (setenv "GHC_PACKAGE_PATH" ghc-package-path-var)
-                (setq hsenv hsenv-dir)
-                (setq hsenv-activated t))
-            (message "An hsenv is already activated (%s)." hsenv)))
-    (message "The environment you provided is not a valid hsenv directory (%s)." hsenv-dir))))
-
-(defun hsenv-activate-default-environment (dir)
-  "Activate the Virtual Haskell Environment named after DIR in directory DIR"
-  (setq dir (file-name-as-directory dir))
-  (let* ((dir-name (directory-file-name dir))
-         (env-name (file-relative-name dir-name (file-name-directory dir-name)))
-         (hsenv-dir-name (concat dir ".hsenv_" env-name))
-         (hsenv-dir (file-name-as-directory hsenv-dir-name)))
-    (hsenv-activate-environment hsenv-dir)))
-
-(defun hsenv-activate (&optional specify-env)
-  "Activate a Virtual Haskell Environment"
-  (interactive "P")
-  (if specify-env
-      (let* ((dir (read-directory-name "Project directory:"))
-             (candidates (file-expand-wildcards (concat dir ".hsenv_*"))))
-        (if candidates
-            (hsenv-activate-environment (completing-read "Environment:" candidates)))
-        (message "Directory (%s) does not contain any hsenv environments."))
-    (let ((dir (read-directory-name "Project directory:")))
-      (hsenv-activate-default-environment dir))))
+  (when (and (hsenv-valid-dirp hsenv-dir)
+             (hsenv-is-not-active))
+    ; Create an hsenv active environment and backup paths
+    (setq hsenv-active-environment (list `(path-backup . ,(getenv "PATH"))
+                                         `(exec-path-backup . ,exec-path)
+                                         `(dir . ,hsenv-dir)))
+    ; Prepend paths
+    (let* ((path-prepend (hsenv-read-file-content hsenv-dir hsenv-path-prepend-file)))
+      (setenv "PATH" (concat  path-prepend ":" (getenv "PATH")))
+      (setq exec-path (append (split-string path-prepend ":") exec-path)))
+    ; Set ghc-package
+    (setenv "GHC_PACKAGE_PATH" (hsenv-read-file-content hsenv-dir hsenv-ghc-package-path-file))))
 
 (defun hsenv-deactivate ()
   "Deactivate the Virtual Haskell Environment"
   (interactive)
-  (if hsenv-activated
-      (progn
-        (setenv "PATH" hsenv-path-backup)
-        (setq exec-path hsenv-exec-path-backup)
-        (setenv "GHC_PACKAGE_PATH" nil)
-        (setq hsenv nil)
-        (setq hsenv-path-backup nil)
-        (setq hsenv-exec-path-backup nil)
-        (setq hsenv-activated nil))
-    (message "No hsenv currently activated.")))
+  (when (hsenv-is-active)
+    ; Restore paths
+    (setenv "PATH" (assoc-default 'path-backup hsenv-active-environment))
+    (setq exec-path (assoc-default 'exec-path-backup hsenv-active-environment))
+    ; Destroy the hsenv active environment
+    (setq hsenv-active-environment nil)))
+
+(defun hsenv-activate-dir (dir)
+  (let ((environments (hsenv-list-environments dir)))
+    (if (null environments)
+        (message "Directory %s does not contain any hsenv.")
+      (let* ((env-name (if (= 1 (length environments))
+                           (car environments)
+                         (completing-read "Environment:" environments)))
+             (hsenv-dir-name (concat dir ".hsenv_" env-name))
+             (hsenv-dir (file-name-as-directory hsenv-dir-name)))
+        (hsenv-activate-environment hsenv-dir)))))
+
+(defun hsenv-list-environments (dir)
+  (let ((hsenv-dirs (file-expand-wildcards (concat dir ".hsenv_*"))))
+    (mapcar (lambda (hsenv-dir) (substring hsenv-dir (+ 7 (length dir)))) hsenv-dirs)))
+
+
+(defun hsenv-activate (&optional select-dir)
+  "Activate a Virtual Haskell Environment"
+  (interactive "P")
+  (if (or select-dir
+          (null (hsenv-list-environments default-directory)))
+      (hsenv-activate-dir (read-directory-name "Directory:"))
+    (hsenv-activate-dir default-directory)))
 
 (provide 'hsenv)
